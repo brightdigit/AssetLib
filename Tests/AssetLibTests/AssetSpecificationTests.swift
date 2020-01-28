3a9fa33d7ec13ccb5adbc22c579bbd857302b48a
Leo Dion <leogdion@brightdigit.com>
2020-01-27 20:58:57 -0500

    fixed lint issue

--- Tests/AssetLibTests/AssetSpecificationTests.swift
+++ Tests/AssetLibTests/AssetSpecificationTests.swift
@@ -3,24 +3,23 @@ import XCTest
 
 final class AssetSpecificationTests: XCTestCase {
   func testExample() {
-    
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct
     // results.
     // XCTAssertEqual(AssetLib().text, "Hello, World!")
-    //print(Bundle.main.urls(forResourcesWithExtension: "json", subdirectory: nil, localization: nil))
-    //print(#file)
+    // print(Bundle.main.urls(forResourcesWithExtension: "json", subdirectory: nil, localization: nil))
+    // print(#file)
     let hereUrl = URL(fileURLWithPath: #file)
     let dataDirectoryUrl = hereUrl.deletingLastPathComponent().appendingPathComponent("Data")
     let enumerator = FileManager.default.enumerator(at: dataDirectoryUrl, includingPropertiesForKeys: nil)
-    
-    let contentsJSONUrls = enumerator!.compactMap{ $0 as? URL }.filter{ $0.pathExtension == "json" }
-    
+
+    let contentsJSONUrls = enumerator!.compactMap { $0 as? URL }.filter { $0.pathExtension == "json" }
+
     let decoder = JSONDecoder()
-    let contentsJSONDatas = contentsJSONUrls.compactMap{
+    let contentsJSONDatas = contentsJSONUrls.compactMap {
       try? Data(contentsOf: $0)
     }
-    let documents = contentsJSONDatas.compactMap{
+    let documents = contentsJSONDatas.compactMap {
       try? decoder.decode(AssetSpecificationDocument.self, from: $0)
     }
   }
