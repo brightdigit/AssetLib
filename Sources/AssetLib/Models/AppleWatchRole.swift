e318004bbddb507a1661e8d0a0880af0959e3789
Leo Dion <leogdion@brightdigit.com>
2020-01-29 14:12:31 -0500

    added comments to enums

--- Sources/AssetLib/Models/AppleWatchRole.swift
+++ Sources/AssetLib/Models/AppleWatchRole.swift
@@ -1,7 +1,14 @@
 public enum AppleWatchRole: String, Codable {
+ /** The icon is used in the notification center. */
   case notificationCenter
+  /**
+  The icon is used in settings.
+   */
   case companionSettings
+  /** The icon is used in the app launcher. */
   case appLauncher
+  /** The icon is used for a long-look notification. */
   case longLook
+  /** The icon is used for a short-look notification. */
   case quickLook
 }
--- Sources/AssetLib/Models/AppleWatchType.swift
+++ Sources/AssetLib/Models/AppleWatchType.swift
@@ -1,6 +1,10 @@
 public enum AppleWatchType: String, Codable {
+  /** The icon is for a 38mm Apple Watch. */
   case size38 = "38mm"
+  /** The icon is for a 40mm Apple Watch. */
   case size40 = "40mm"
+  /** The icon is for a 42mm Apple Watch. */
   case size42 = "42mm"
+  /** The icon is for a 44mm Apple Watch. */
   case size44 = "44mm"
 }
