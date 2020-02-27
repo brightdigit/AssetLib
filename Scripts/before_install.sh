7542c3227912770ff1f85a3a088cf5ce44195fae
leogdion <leogdion@brightdigit.com>
2020-02-26 19:19:35 -0500

    check for linux os name

--- Scripts/before_install.sh
+++ Scripts/before_install.sh
@@ -3,9 +3,8 @@
 if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
   brew update
   brew bundle
-else
+elif [[ $TRAVIS_OS_NAME = 'linux' ]]; then
   wget https://swift.org/builds/swift-5.1.3-release/ubuntu1804/swift-5.1.3-RELEASE/swift-5.1.3-RELEASE-ubuntu18.04.tar.gz
   tar xzf swift-5.1.3-RELEASE-ubuntu18.04.tar.gz
-  ls ${PWD}/swift-5.1.3-RELEASE-ubuntu18.04/usr/bin
   export PATH="${PWD}/swift-5.1.3-RELEASE-ubuntu18.04/usr/bin:$PATH"
 fi
