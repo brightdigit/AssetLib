2c750e9a1c99a489b3f3c56c1aa967cb76ef500d
leogdion <leogdion@brightdigit.com>
2020-02-26 21:31:48 -0500

    fixing version number

--- Scripts/before_install.sh
+++ Scripts/before_install.sh
@@ -6,7 +6,7 @@ if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
   brew update
   brew bundle
 elif [[ $TRAVIS_OS_NAME = 'linux' ]]; then
-  RELEASE_DOT=$(lsb_release -r)
+  RELEASE_DOT=$(lsb_release -sr)
   RELEASE_NUM=${RELEASE_DOT//[-._]/}
   wget https://swift.org/builds/swift-${SWIFT_VER}-release/ubuntu${RELEASE_NUM}/swift-${SWIFT_VER}-RELEASE/swift-${SWIFT_VER}-RELEASE-ubuntu${RELEASE_DOT}.tar.gz
   tar xzf swift-${SWIFT_VER}-RELEASE-ubuntu${RELEASE_DOT}.tar.gz
