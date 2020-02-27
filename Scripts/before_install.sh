9865e64c976c9cb29a449948eaf0ed0bc2e0717b
leogdion <leogdion@brightdigit.com>
2020-02-26 19:33:23 -0500

    use variables for ubuntu dist and swift version

--- Scripts/before_install.sh
+++ Scripts/before_install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+SWIFT_VER="5.1.3"
+
 if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
   brew update
   brew bundle
 elif [[ $TRAVIS_OS_NAME = 'linux' ]]; then
-  wget https://swift.org/builds/swift-5.1.3-release/ubuntu1804/swift-5.1.3-RELEASE/swift-5.1.3-RELEASE-ubuntu18.04.tar.gz
-  tar xzf swift-5.1.3-RELEASE-ubuntu18.04.tar.gz
-  export PATH="${PWD}/swift-5.1.3-RELEASE-ubuntu18.04/usr/bin:$PATH"
+  RELEASE_DOT=$(lsb_release -r)
+  RELEASE_NUM=$(cut -f2 <<< "$RELEASE_DOT")
+  wget https://swift.org/builds/swift-$SWIFT_VER-release/ubuntu$RELEASE_NUM/swift-$SWIFT_VER-RELEASE/swift-$SWIFT_VER-RELEASE-ubuntu$RELEASE_DOT.tar.gz
+  tar xzf swift-$SWIFT_VER-RELEASE-ubuntu$RELEASE_DOT.tar.gz
+  export PATH="${PWD}/swift-$SWIFT_VER-RELEASE-ubuntu$RELEASE_DOT/usr/bin:$PATH"
 fi
