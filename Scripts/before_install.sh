fe37da3bc63109ed7ad0c2d93fb917db4fbab0da
leogdion <leogdion@brightdigit.com>
2020-02-26 21:26:14 -0500

    fixing variables names

--- Scripts/before_install.sh
+++ Scripts/before_install.sh
@@ -8,6 +8,6 @@ if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
 elif [[ $TRAVIS_OS_NAME = 'linux' ]]; then
   RELEASE_DOT=$(lsb_release -r)
   RELEASE_NUM=${RELEASE_DOT//[-._]/}
-  wget https://swift.org/builds/swift-$SWIFT_VER-release/ubuntu$RELEASE_NUM/swift-$SWIFT_VER-RELEASE/swift-$SWIFT_VER-RELEASE-ubuntu$RELEASE_DOT.tar.gz
-  tar xzf swift-$SWIFT_VER-RELEASE-ubuntu$RELEASE_DOT.tar.gz
+  wget https://swift.org/builds/swift-${SWIFT_VER}-release/ubuntu${RELEASE_NUM}/swift-${SWIFT_VER}-RELEASE/swift-${SWIFT_VER}-RELEASE-ubuntu${RELEASE_DOT}.tar.gz
+  tar xzf swift-${SWIFT_VER}-RELEASE-ubuntu${RELEASE_DOT}.tar.gz
 fi
