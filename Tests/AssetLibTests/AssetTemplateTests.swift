cc0c6d6c23b16208b6584c6d666969e578e6454b
Leo Dion <leogdion@brightdigit.com>
2020-03-14 14:53:14 -0400

    adding template test

---
+++ Tests/AssetLibTests/AssetTemplateTests.swift
@@ -0,0 +1,37 @@
+@testable import AssetLib
+import XCTest
+
+final class AssetTemplateTests: XCTestCase {
+  func testTemplate () {
+    //var template = AssetTemplate() {
+      /**
+       # Image Set
+       * Render As
+          * default
+          * origin image
+          * template
+        * Compression
+        * Resizing
+        * Devices (bitflag)
+        * Appearance
+        * High Contrast
+        * Scales
+        * Gamut
+        * Direction
+        * Width Class
+        * Height class
+        * Memory
+        * Graphics
+        * Apple Watch (width)
+        * Auto Scaling
+        * Locale
+        * Resource Tags
+       
+       # App Icon
+          * Devices (bitflag)
+          * Gamut
+          * pre-rendered
+       */
+    //}
+  }
+}
