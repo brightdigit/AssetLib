7e90a963f18c1ce1f3b957b8b54b73078532b6b0
Leo Dion <leogdion@brightdigit.com>
2020-03-18 14:58:25 -0400

    working on organizing tests

--- Tests/AssetLibTests/Tests/AssetTemplateTests.swift
+++ Tests/AssetLibTests/Tests/AssetTemplateTests.swift
@@ -2,25 +2,35 @@
 import XCTest
 
 final class AssetTemplateTests: XCTestCase {
+  fileprivate func assertImageSet(_ imageSetName: String, _ template: ImageSetTemplate) {
+    let hereUrl = URL(fileURLWithPath: #file)
+    let dataDirectoryUrl = hereUrl.deletingLastPathComponent().appendingPathComponent("../../../Data/Data")
+
+    let bigImageSetUrl = dataDirectoryUrl.appendingPathComponent(imageSetName).appendingPathComponent("Contents.json")
+
+    let builder = ImageSetTemplateBuilder()
+
+    let actual = builder.document(fromTemplate: template) as! AssetSpecificationDocument
+    XCTAssertNotNil(actual.images)
+
+    let expectedData = try! Data(contentsOf: bigImageSetUrl)
+    let encoder = JSONEncoder()
+    encoder.outputFormatting = .prettyPrinted
+
+    let actualData = try! encoder.encode(actual)
+    let decoder = JSONDecoder()
+    let expected = try! decoder.decode(AssetSpecificationDocument.self, from: expectedData)
+
+    XCTAssertEqual(expected.images?.count, actual.images?.count)
+    let mismatch = Data.jsonMismatch(lhs: actualData, rhs: expectedData)
+    XCTAssert(mismatch.isEmpty)
+
+    if !mismatch.isEmpty {
+      try? actualData.write(to: hereUrl.deletingLastPathComponent().appendingPathComponent("../../../unmatched.json"))
+    }
+  }
+
   func testTemplate() {
-    _ = ImageSetTemplate(
-      renderAs: .template,
-      compression: .gpuOptimizedBest,
-      preserveVectorData: true,
-      devices: Set([.universal, .iphone, .ipad, .macCatalyst, .watch]),
-      appearances: [ValueAppearance(value: Luminosity.dark).eraseToAny(), ValueAppearance(value: Luminosity.light).eraseToAny()],
-      scaling: .individual,
-      specifyGamut: true,
-      direction: [],
-      specifiedWidthClass: nil,
-      specifiedHeightClass: nil,
-      memorySet: [.requires1GB, .requires2GB, .requires4GB],
-      graphicFSSet: [.metal3v1, .metal4v1, .apple6],
-      specifyAWWidth: true,
-      autoScaling: true,
-      locales: [],
-      resourceTags: []
-    )
     let template = ImageSetTemplate(
       renderAs: .template,
       compression: .gpuOptimizedBest,
@@ -39,34 +49,7 @@ final class AssetTemplateTests: XCTestCase {
       locales: [],
       resourceTags: []
     )
-    let hereUrl = URL(fileURLWithPath: #file)
-    let dataDirectoryUrl = hereUrl.deletingLastPathComponent().appendingPathComponent("../../../Data/Data")
-
-    let bigImageSetUrl = dataDirectoryUrl.appendingPathComponent("SingleImage.imageset").appendingPathComponent("Contents.json")
-
-    let builder = ImageSetTemplateBuilder()
-
-    let actual = builder.document(fromTemplate: template) as! AssetSpecificationDocument
-    XCTAssertNotNil(actual.images)
-
-    let expectedData = try! Data(contentsOf: bigImageSetUrl)
-    let encoder = JSONEncoder()
-    encoder.outputFormatting = .prettyPrinted
-
-    let actualData = try! encoder.encode(actual)
-    let decoder = JSONDecoder()
-    let expected = try! decoder.decode(AssetSpecificationDocument.self, from: expectedData)
-
-    guard let count = actual.images?.count else {
-      XCTFail("Missing images")
-      return
-    }
-    XCTAssertEqual(expected.images?.count, actual.images?.count)
-    let mismatch = Data.jsonMismatch(lhs: actualData, rhs: expectedData)
-    XCTAssert(mismatch.isEmpty)
-
-    if !mismatch.isEmpty {
-      try? actualData.write(to: hereUrl.deletingLastPathComponent().appendingPathComponent("../../../unmatched.json"))
-    }
+    let imageSetName = "SingleImage.imageset"
+    assertImageSet(imageSetName, template)
   }
 }
