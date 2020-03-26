149dbeda6d9a701b99527f3c53924d1b8757e716
Leo Dion <leogdion@brightdigit.com>
2020-03-26 17:22:25 -0400

    added templatetypeparser tests

--- Tests/AssetLibTests/Tests/TemplateTypeParserTests.swift
+++ Tests/AssetLibTests/Tests/TemplateTypeParserTests.swift
@@ -4,5 +4,8 @@ import XCTest
 final class TemplateDetailsFactoryTests: XCTestCase {
   func testDetails() {
     let parser = TemplateTypeParser()
+    XCTAssertEqual(parser.templateType(fromPath: "Image.imageset"), .imageset)
+    XCTAssertEqual(parser.templateType(fromPath: "Image.appiconset"), .appicon)
+    XCTAssertEqual(parser.templateType(fromPath: UUID().uuidString), nil)
   }
 }
