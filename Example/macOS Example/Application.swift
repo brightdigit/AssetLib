4e04be5069c194c1572002d353f2100d7ffae406
Leo Dion <leogdion@brightdigit.com>
2020-10-19 10:19:44 -0400

    fixing CI issues

--- Example/macOS Example/Application.swift
+++ Example/macOS Example/Application.swift
@@ -8,6 +8,7 @@ public class Application: NSApplication {
     delegate = appDelegateContainer
   }
 
+  @available(*, unavailable)
   required init?(coder _: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
