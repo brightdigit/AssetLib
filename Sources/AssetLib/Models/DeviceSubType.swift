60f88d1c0c4e3a255a8ac897216eadf174b437fa
Leo Dion <leogdion@brightdigit.com>
2021-09-23 09:11:05 -0400

    adding 41mm and 45mm

--- Sources/AssetLib/Models/DeviceSubType.swift
+++ Sources/AssetLib/Models/DeviceSubType.swift
@@ -4,10 +4,14 @@ public enum DeviceSubType: String, Codable {
   case size38 = "38mm"
   /** The icon is for a 40mm Apple Watch. */
   case size40 = "40mm"
+  /** The icon is for a 41mm Apple Watch. */
+  case size41 = "41mm"
   /** The icon is for a 42mm Apple Watch. */
   case size42 = "42mm"
   /** The icon is for a 44mm Apple Watch. */
   case size44 = "44mm"
+  /** The icon is for a 45mm Apple Watch. */
+  case size45 = "45mm"
   /** For ImageSets which will be used in mac Catalyst apps.  */
   case macCatalyst = "mac-catalyst"
 }
