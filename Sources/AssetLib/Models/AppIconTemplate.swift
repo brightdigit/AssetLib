aa50def95ca3d8d2bb960f72ca9b9478e142daca
Leo Dion <leogdion@brightdigit.com>
2022-05-08 21:13:05 -0400

    [skip ci] WIP

--- Sources/AssetLib/Models/AppIconTemplate.swift
+++ Sources/AssetLib/Models/AppIconTemplate.swift
@@ -1,27 +1,154 @@
-protocol Rule: Codable, Equatable {}
 
-struct AnyRule: Codable, Equatable {}
+protocol RuleDecodable: RuleProtocol {
+  static func canDecodeType(_ type: String) -> Bool
+  init(from decoder: Decoder) throws
+  
+}
 
-public struct FieldRule: Rule, Codable, Equatable {}
 
-public struct CollectionRule: Rule, Codable, Equatable {}
+protocol TypedRuleDecodable {
+  associatedtype OperatorType : RawRepresentable where OperatorType.RawValue == String
+}
 
-public struct SourceRule: Codable, Equatable {
-  public enum RuleType: String, Codable {
-    case equals
-    case and
-    case or
+extension TypedRuleDecodable {
+  static func canDecodeType(_ type: String) -> Bool {
+    OperatorType(rawValue: type) != nil
   }
+}
+ 
+extension RuleDecodable {
+  static func anyRule(from decocder: Decoder) throws -> AnyRule {
+    let rule = try Self.init(from: decocder)
+    return AnyRule(wrapped: rule)
+  }
+}
+protocol RuleProtocol {
+  
+}
 
-  public enum Field: String {
+extension RuleProtocol {
+  func equals (rhs: RuleProtocol) -> Bool {
+    return false
+  }
+}
+
+extension RuleProtocol where Self : Equatable {
+  func equals (rhs: Self) -> Bool {
+    self == rhs
+  }
+  
+  func equals<RHSType: RuleProtocol>(rhs: RHSType) -> Bool {
+    guard let rhs = rhs as? Self else {
+      return false
+    }
+    return self == rhs
+  }
+}
+protocol Rule: Codable, Equatable, RuleProtocol {}
+
+struct AnyRule {
+  
+  let wrapped: RuleProtocol
+}
+
+extension AnyRule : Equatable {
+  static func == (lhs: AnyRule, rhs: AnyRule) -> Bool {
+    lhs.wrapped.equals(rhs: rhs.wrapped)
+  }
+  
+  
+}
+
+enum Rules {
+  static let types : [RuleDecodable.Type] = [FieldRule.self, CollectionRule.self]
+}
+
+public struct FieldRule: Rule, Codable, Equatable, TypedRuleDecodable {
+
+  
+  let type : OperatorType
+  let field : Field
+  let value : String
+
+  public enum Field: String, Codable {
     case locale
     case idiom
     case heightClass
     case widthClass
   }
+  
+  enum OperatorType: String, Codable  {
+    case equals
+  }
+}
 
-  let type: RuleType
+public struct CollectionRule: Rule, Codable, Equatable,TypedRuleDecodable {
+  enum CodingKeys : String, CodingKey {
+    case type
+    case rules
+//    case field
+//    case value
+//    case rules
+  }
+  
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: Self.CodingKeys)
+    //self.type = type
+    
+    var rules = [AnyRule].init()
+    var rulescontainer  = try container.nestedUnkeyedContainer(forKey: .rules)
+    while !rulescontainer.isAtEnd {
+      let container = try rulescontainer.nestedContainer(keyedBy: SourceRule.CodingKeys.self)
+      let ruleType = try container.decode(String.self, forKey: .type)
+      let decodableType = Rules.types.first { type in
+        type.canDecodeType(ruleType)
+      }
+      guard let decodableType = decodableType else {
+        fatalError()
+      }
+
+      rules.append(try decodableType.anyRule(from: decoder))
+    }
+  }
+  let type : OperatorType
+  let rules : [AnyRule]
+  
+  enum OperatorType: String, Codable {
+    case and
+    case or
+  }
+}
+
+public struct SourceRule: Codable, Equatable {
+//  public enum RuleType: String, Codable {
+//    case equals
+//    case and
+//    case or
+//  }
+//
+//  let type: String
   let rule: AnyRule
+
+  
+  enum CodingKeys : String, CodingKey {
+    case type
+//    case field
+//    case value
+//    case rules
+  }
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    let ruleType = try container.decode(String.self, forKey: .type)
+    let decodableType = Rules.types.first { type in
+      type.canDecodeType(ruleType)
+    }
+    guard let decodableType = decodableType else {
+      fatalError()
+    }
+
+    let rule = try decodableType.anyRule(from: decoder)
+    self.rule = rule
+  }
 }
 
 public struct SourceProvider: Codable, Equatable {
